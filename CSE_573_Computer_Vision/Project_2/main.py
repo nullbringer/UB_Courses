1ede2a7f1fc2638b7a21f30f9306cac781fccf0d
urbangeek <gupta.amlan@gmail.com>
2018-11-03 06:19:42 -0400

    cvip

--- CSE_573_Computer_Vision/Project_2/main.py
+++ CSE_573_Computer_Vision/Project_2/main.py
@@ -20,33 +20,20 @@ def write_image(img, image_name):
 	cv2.imwrite(OUTPUT_FOLDER + image_name,img)
 
 
-def image_feature_and_homography():
+def perform_sift(img):
 
-	#### Mark Keypoints ####
-
-	mountain_1_img = cv2.imread(SOURCE_FOLDER + "mountain1.jpg", 0)
-	mountain_2_img = cv2.imread(SOURCE_FOLDER + "mountain2.jpg", 0)
-
-	kp_mountain_1_img = np.zeros(mountain_1_img.shape)
+	sift_img = np.zeros(img.shape)
 
 
 	sift = cv2.xfeatures2d.SIFT_create()
 	
-	kp_1, desc_1 = sift.detectAndCompute(mountain_1_img, None)
-	kp_mountain_1_img = cv2.drawKeypoints(mountain_1_img, kp_1, kp_mountain_1_img)
+	kp, desc = sift.detectAndCompute(img, None)
+	sift_img = cv2.drawKeypoints(img, kp, sift_img)
 
-	write_image(kp_mountain_1_img, 'task1_sift1.jpg')
+	return sift_img, kp, desc
 
-	kp_mountain_2_img = np.zeros(mountain_2_img.shape)
+def find_knn_match(img_1, img_2, kp_1, kp_2, desc_1, desc_2):
 
-	kp_2, desc_2 = sift.detectAndCompute(mountain_2_img, None)
-	kp_mountain_2_img = cv2.drawKeypoints(mountain_2_img, kp_2, kp_mountain_2_img)
-
-	write_image(kp_mountain_2_img, 'task1_sift2.jpg')
-
-
-
-	### Draw matches using k nearest neighbors ##
 	bf = cv2.BFMatcher()
 	matches = bf.knnMatch(desc_1,desc_2, k=2)
 
@@ -57,12 +44,12 @@ def image_feature_and_homography():
 	        good_matches.append(m)
 
 
-	knn_matched_img = np.zeros(mountain_1_img.shape)
-	knn_matched_img = cv2.drawMatches(mountain_1_img, kp_1, mountain_2_img, kp_2, good_matches, knn_matched_img, flags=2)
+	knn_matched_img = np.zeros(img_1.shape)
+	knn_matched_img = cv2.drawMatches(img_1, kp_1, img_2, kp_2, good_matches, knn_matched_img, flags=2)
+	return knn_matched_img, good_matches
 
-	write_image(knn_matched_img, 'task1_matches_knn.jpg')
 
-	### Calculate H matrix ###
+def find_homography_and_match_images(good_matches, kp_1, kp_2, img_1, img_2):
 
 	src_pts = np.float32([ kp_1[m.queryIdx].pt for m in good_matches ]).reshape(-1,1,2)
 	dst_pts = np.float32([ kp_2[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
@@ -70,56 +57,134 @@ def image_feature_and_homography():
 	M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 	matchesMask = mask.ravel().tolist()
 
-	print(M)
-
 	draw_params = dict(matchColor = (0,255,0),
 	           singlePointColor = None,
 	           matchesMask = matchesMask,
 	           flags = 2)
 
 
-	task1_matches = cv2.drawMatches(mountain_1_img, kp_1, mountain_2_img, kp_2, good_matches, None, **draw_params)
+	matches_img = cv2.drawMatches(img_1, kp_1, img_2, kp_2, good_matches, None, **draw_params)
+	return M, matches_img
+
+
+
+def do_image_stitching(img_1, img_2, M):
+
+	(h1, w1) = img_1.shape[:2]
+	(h2, w2) = img_2.shape[:2]
+
+	#remap the coordinates of the projected image onto the panorama image space
+	top_left = np.dot(M,np.asarray([0,0,1]))
+	top_right = np.dot(M,np.asarray([w2,0,1]))
+	bottom_left = np.dot(M,np.asarray([0,h2,1]))
+	bottom_right = np.dot(M,np.asarray([w2,h2,1]))
+
+	#normalize
+	top_left = top_left/top_left[2]
+	top_right = top_right/top_right[2]
+	bottom_left = bottom_left/bottom_left[2]
+	bottom_right = bottom_right/bottom_right[2]
 
 	
+
+	pano_left = int(min(top_left[0], bottom_left[0], 0))
+	pano_right = int(max(top_right[0], bottom_right[0], w1))
+	W = pano_right - pano_left
+
+	pano_top = int(min(top_left[1], top_right[1], 0))
+	pano_bottom = int(max(bottom_left[1], bottom_right[1], h1))
+	H = pano_bottom - pano_top
+
+	size = (W, H)
+
+	# offset of first image relative to panorama
+	X = int(min(top_left[0], bottom_left[0], 0))
+	Y = int(min(top_left[1], top_right[1], 0))
+	offset = (-X, -Y)
+
+	panorama = np.zeros((size[1], size[0]), np.uint8)
+
+	(ox, oy) = offset
+
+	translation = np.matrix([
+					[1.0, 0.0, ox],
+					[0, 1.0, oy],
+					[0.0, 0.0, 1.0]
+					])
+
+
+	M = translation * M
+
+	cv2.warpPerspective(img_1, M, size, panorama)
+
+	panorama[oy:h1+oy, ox:ox+w1] = img_2
+
+	return panorama  
+
+
+
+
+
+
+def image_feature_and_homography(mountain_1_img, mountain_2_img):
+
+	# task 1.1
+
+	kp_mountain_1_img, kp_1, desc_1 = perform_sift(mountain_1_img)
+	write_image(kp_mountain_1_img, 'task1_sift1.jpg')
+
+	
+	kp_mountain_2_img, kp_2, desc_2 = perform_sift(mountain_2_img)
+	write_image(kp_mountain_2_img, 'task1_sift2.jpg')
+
+
+	# task 1.2
+	
+	knn_matched_img, good_matches = find_knn_match(mountain_1_img, mountain_2_img, kp_1, kp_2, desc_1, desc_2)
+	write_image(knn_matched_img, 'task1_matches_knn.jpg')
+
+	
+	#task 1.3, task 1.4
+	
+	h_matrix, task1_matches = find_homography_and_match_images(good_matches, kp_1, kp_2, mountain_1_img, mountain_2_img)
+	print(h_matrix)
 	write_image(task1_matches, 'task1_matches.jpg')
 
-
-	im_out = cv2.warpPerspective(mountain_1_img, M, (mountain_2_img.shape[1],mountain_2_img.shape[0]))
-	task1_pano = cv2.addWeighted(im_out, 0.2, mountain_2_img, 0.8, 1)
-
-	write_image(task1_pano, 'task1_pano.jpg')
+	# task 1.5
+	panorama = do_image_stitching(mountain_1_img, mountain_2_img, h_matrix)
+	write_image(panorama,'task1_pano.jpg')
 
 
+def epipolar_geometry(tsucuba_left_img, tsucuba_right_img):
 
-	################
+	# task 2.1
 
-	# mountain_1_canvas = np.zeros([mountain_1_img.shape[0]*2,mountain_1_img.shape[1]*2])
+	kp_tsucuba_left_img, kp_1, desc_1 = perform_sift(tsucuba_left_img)
+	write_image(kp_tsucuba_left_img, 'task2_sift1.jpg')
 
-	# x_offset= 300
-	# y_offset=200
-	# mountain_1_canvas[y_offset : y_offset + mountain_1_img.shape[0], x_offset : x_offset + mountain_1_img.shape[1]] = mountain_1_img
-
-	# # write_image(mountain_1_canvas,'jjjhjh.jpg')
-
-	# mountain_2_canvas = np.zeros([mountain_2_img.shape[0]*2,mountain_2_img.shape[1]*2])
-
-	# x_offset= 500
-	# y_offset=200
-	# mountain_2_canvas[y_offset : y_offset + mountain_2_img.shape[0], x_offset : x_offset + mountain_2_img.shape[1]] = mountain_2_img
-
-
-	# im_out = cv2.warpPerspective(mountain_1_canvas, M, (mountain_2_canvas.shape[1],mountain_2_canvas.shape[0]))
-	# task1_pano_ddfdf = cv2.addWeighted(im_out, 1, mountain_2_canvas, 1, 1)
-
-	# write_image(task1_pano_ddfdf, 'task1_panosafsafsd.jpg')
-
+	kp_tsucuba_right_img, kp_2, desc_2 = perform_sift(tsucuba_right_img)
+	write_image(kp_tsucuba_right_img, 'task2_sift2.jpg')
+	
+	knn_matched_img, good_matches = find_knn_match(tsucuba_left_img, tsucuba_right_img, kp_1, kp_2, desc_1, desc_2)
+	write_image(knn_matched_img, 'task2_matches_knn.jpg')
 
 
 
 
 
 def main():
-	image_feature_and_homography()
-	print('Done!!!!')
+
+	mountain_1_img = cv2.imread(SOURCE_FOLDER + "mountain1.jpg", 0)
+	mountain_2_img = cv2.imread(SOURCE_FOLDER + "mountain2.jpg", 0)
+	
+	image_feature_and_homography(mountain_1_img, mountain_2_img)
+
+	tsucuba_left_img = cv2.imread(SOURCE_FOLDER + "tsucuba_left.png", 0)
+	tsucuba_right_img = cv2.imread(SOURCE_FOLDER + "tsucuba_right.png", 0)
+
+	epipolar_geometry(tsucuba_left_img, tsucuba_right_img)
+	
+
+
 
 main()
