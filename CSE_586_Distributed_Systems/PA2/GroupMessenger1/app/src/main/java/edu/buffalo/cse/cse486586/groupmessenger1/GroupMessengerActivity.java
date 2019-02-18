05f0b61017fa9882211d10e7bcfd3798cef7609a
urbangeek <gupta.amlan@gmail.com>
2019-02-18 13:40:18 -0500

    PA2

--- CSE_586_Distributed_Systems/PA2/GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
+++ CSE_586_Distributed_Systems/PA2/GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
@@ -1,13 +1,30 @@
 package edu.buffalo.cse.cse486586.groupmessenger1;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * GroupMessengerActivity is the main Activity for the assignment.
@@ -18,49 +35,77 @@ import android.widget.TextView;
 public class GroupMessengerActivity extends Activity {
 
     private static final String TAG = GroupMessengerActivity.class.getName();
+    static final int SERVER_PORT = 10000;
+    private static AtomicInteger seqId = new AtomicInteger(0);
+    private static final String KEY_FIELD = "key";
+    private static final String VALUE_FIELD = "value";
+    private static final String REMOTE_PORT [] = { "11108",  "11112", "11116", "11120", "11124"};
+
+    private Uri mUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_messenger);
 
-        /*
-         * TODO: Use the TextView to display your messages. Though there is no grading component
-         * on how you display the messages, if you implement it, it'll make your debugging easier.
-         */
+
         final TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
-        
-        /*
-         * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
-         * OnPTestClickListener demonstrates how to access a ContentProvider.
-         */
-        findViewById(R.id.button1).setOnClickListener(
-                new OnPTestClickListener(tv, getContentResolver()));
-        
-        /*
-         * TODO: You need to register and implement an OnClickListener for the "Send" button.
-         * In your implementation you need to get the message from the input box (EditText)
-         * and send it to other AVDs.
-         */
+
+
 
         final EditText editText = (EditText) findViewById(R.id.editText1);
+
+        mUri = buildUri("content", "edu.buffalo.cse.cse486586.groupmessenger1.provider");
+
+
+        TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
+
+        try {
+
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+
+        } catch (IOException e) {
+
+            Log.e(TAG, "Can't create a ServerSocket");
+            return;
+        }
+
+
+        findViewById(R.id.button1).setOnClickListener(
+                new OnPTestClickListener(tv, getContentResolver()));
 
         findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e(TAG,"Clicked Send");
 
 
                 String msg = editText.getText().toString() + "\n";
                 editText.setText(""); // This is one way to reset the input box.
 
-                tv.append("\t" + msg); // This is one way to display a string.
+                Long tsLong = System.currentTimeMillis()/1000;
+                String ts = tsLong.toString();
+
+                ContentValues mContentValues = new ContentValues();
+
+                mContentValues.put(KEY_FIELD, ts);
+                mContentValues.put(VALUE_FIELD, msg);
+
+
+                getContentResolver().insert(mUri, mContentValues);
+
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
 
                 Log.e(TAG,msg);
 
             }
         });
+
+
     }
 
     @Override
@@ -69,4 +114,143 @@ public class GroupMessengerActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_group_messenger, menu);
         return true;
     }
+
+
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            ServerSocket serverSocket = sockets[0];
+
+            /* infinite while loop to accept multiple messeges */
+
+            while (true) {
+
+                try {
+
+
+                    Socket clientSocket = serverSocket.accept();
+
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+                    String incomingMessege = bufferedReader.readLine();
+
+                    if (incomingMessege != null) {
+
+                        publishProgress(incomingMessege);
+                    }
+
+                    clientSocket.close();
+
+
+                } catch (IOException e) {
+                    Log.e(TAG, "Client Connection failed");
+                }
+            }
+
+
+        }
+
+        protected void onProgressUpdate(String...strings) {
+            /*
+             * The following code displays what is received in doInBackground().
+             */
+            String strReceived = strings[0].trim();
+            final TextView tv = (TextView) findViewById(R.id.textView1);
+
+
+            String key = Integer.toString(seqId.getAndIncrement());
+
+
+
+            ContentValues mContentValues = new ContentValues();
+
+            mContentValues.put(KEY_FIELD, key);
+            mContentValues.put(VALUE_FIELD, strReceived);
+
+            getContentResolver().insert(mUri, mContentValues);
+
+
+            tv.append(strReceived+ "\n");
+
+//
+//            Cursor resultCursor = getContentResolver().query(mUri, null, key, null, null);
+//
+//            if (resultCursor == null) {
+//                Log.e(TAG, "Result null");
+//            }
+//
+//            int keyIndex = resultCursor.getColumnIndex(KEY_FIELD);
+//            int valueIndex = resultCursor.getColumnIndex(VALUE_FIELD);
+//            if (keyIndex == -1 || valueIndex == -1) {
+//                Log.e(TAG, "Wrong columns");
+//                resultCursor.close();
+//            }
+//
+//            resultCursor.moveToFirst();
+//
+//            if (!(resultCursor.isFirst() && resultCursor.isLast())) {
+//                Log.e(TAG, "Wrong number of rows");
+//                resultCursor.close();
+//            }
+//
+//
+//            String returnValue = resultCursor.getString(valueIndex);
+//
+//
+//            tv.append(returnValue+ "\n");
+//
+//
+//            resultCursor.close();
+
+
+            return;
+        }
+    }
+
+
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... msgs) {
+            try {
+
+
+                for (int z=0; z<5; z++) {
+
+                    Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                            Integer.parseInt(REMOTE_PORT[z]));
+
+
+                    String msgToSend = msgs[0];
+
+                    DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                    dataOutputStream.writeBytes(msgToSend);
+                    dataOutputStream.flush();
+
+                    socket.close();
+                    dataOutputStream.close();
+                }
+
+
+            } catch (UnknownHostException e) {
+                Log.e(TAG, "ClientTask UnknownHostException");
+            } catch (IOException e) {
+                Log.e(TAG, "ClientTask socket IOException");
+            }
+
+            return null;
+        }
+    }
+
+    private static Uri buildUri(String scheme, String authority) {
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.authority(authority);
+        uriBuilder.scheme(scheme);
+        return uriBuilder.build();
+    }
+
+
 }
--- CSE_586_Distributed_Systems/PA2/GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerProvider.java
+++ CSE_586_Distributed_Systems/PA2/GroupMessenger1/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerProvider.java
@@ -35,6 +35,7 @@ public class GroupMessengerProvider extends ContentProvider {
     private static final String VALUE_FIELD = "value";
     private static final String PREFERENCE_FILE = "groupMsgPreferenceFile";
 
+
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // You do not need to implement this.
@@ -49,17 +50,6 @@ public class GroupMessengerProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        /*
-         * TODO: You need to implement this method. Note that values will have two columns (a key
-         * column and a value column) and one row that contains the actual (key, value) pair to be
-         * inserted.
-         * 
-         * For actual storage, you can use any option. If you know how to use SQL, then you can use
-         * SQLite. But this is not a requirement. You can use other storage options, such as the
-         * internal storage option that we used in PA1. If you want to use that option, please
-         * take a look at the code for PA1.
-         */
-
 
 
 
@@ -88,17 +78,7 @@ public class GroupMessengerProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        /*
-         * TODO: You need to implement this method. Note that you need to return a Cursor object
-         * with the right format. If the formatting is not correct, then it is not going to work.
-         *
-         * If you use SQLite, whatever is returned from SQLite is a Cursor object. However, you
-         * still need to be careful because the formatting might still be incorrect.
-         *
-         * If you use a file storage option, then it is your job to build a Cursor * object. I
-         * recommend building a MatrixCursor described at:
-         * http://developer.android.com/reference/android/database/MatrixCursor.html
-         */
+
 
         Log.v("query", selection);
 
